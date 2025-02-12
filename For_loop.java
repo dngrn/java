78a55ac57d8a69f6312ccf738c13fa7e663867f4
dngrn <147801516+dngrn@users.noreply.github.com>
2025-02-12 16:08:27 +0530

    Add files via upload

---
+++ For_loop.java
@@ -0,0 +1,23 @@
+
+public class For_loop
+{
+	public static void main(String[] args) {
+    int a=7;
+    int i=1;
+    for(; ;)
+     {
+         if(a>3)
+         {
+             System.out.println(a);
+             
+         }
+         else{
+             break;
+         }
+         a--;
+     }
+	    
+	    
+	    
+	}
+}
