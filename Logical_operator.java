8c33e21642142840e256df513e5f3e677fbbac1e
dngrn <147801516+dngrn@users.noreply.github.com>
2025-02-12 11:54:35 +0530

    Add files via upload

---
+++ Logical_operator.java
@@ -0,0 +1,15 @@
+class Logical_operator
+{
+	public static void main(String[] args) {
+
+   int a=5,b=6,c=7;
+   
+   System.out.println(a>b);
+   System.out.println(a<b);
+   System.out.println(c<b);
+   System.out.println(b>a);
+   System.out.println(b>c);
+   System.out.println(c>a);
+   System.out.println(c>b);
+	}
+}
