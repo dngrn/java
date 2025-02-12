84c8f249d7c0f2c32a7fae5e42ad25116ce57f57
dngrn <147801516+dngrn@users.noreply.github.com>
2025-02-12 11:09:16 +0530

    Add files via upload

---
+++ Arithmetic_operator.java
@@ -0,0 +1,24 @@
+
+class Arithmetic_operator
+{
+	public static void main(String[] args) {
+		byte a=225;
+		byte b=330;
+		
+		int sum=a+b;
+		System.out.println(sum);
+		
+		int subtract=a-b;
+		System.out.println(subtract);
+		
+		int multiply=a*b;
+		System.out.println(multiply);
+		
+		int division=a/b;
+		System.out.println(division);
+		
+		int modulo=a%b;
+		System.out.println(modulo);
+		
+	}
+}
