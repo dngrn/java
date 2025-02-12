eb27f0939cc92ec3ca217fc57304440906455de5
dngrn <147801516+dngrn@users.noreply.github.com>
2025-02-12 12:51:48 +0530

    Add files via upload

---
+++ Assignment_operator.java
@@ -0,0 +1,18 @@
+class Assignment_operator
+{
+	public static void main(String[] args) {
+
+   int a=492;
+   int b=333;
+   int c=161;
+   a+=b;
+   b*=a;
+   c-=a;
+   
+   System.out.println(a);
+   System.out.println(b);
+   System.out.println(c);
+
+
+	}
+}
