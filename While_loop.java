3826147f659730a0bc35ff401cbca7ecb7db55c5
dngrn <147801516+dngrn@users.noreply.github.com>
2025-02-12 16:15:00 +0530

    Add files via upload

---
+++ While_loop.java
@@ -0,0 +1,17 @@
+
+public class While_loop
+{
+	public static void main(String[] args) {
+    int a=7;
+    int i=1;
+    while(i>0)
+     {
+         i=4;
+         System.out.println(i--);
+         i--;
+         
+         
+     }
+	    
+	}
+}
