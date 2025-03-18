b544ed95168bd9e30b1b440592554ccf511ae117
dngrn <147801516+dngrn@users.noreply.github.com>
2025-03-18 12:59:27 +0530

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class BubbleSort
+{
+	public static void main(String[] args) {
+    
+    int a[]={9,7,1,5};
+    
+    for(int i=0;i<a.length-1;i++)
+    {
+        for(int j=0;j<a.length-1;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                int temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+
+        }
+
+    }
+    for(int i=0;i<a.length;i++)
+    {
+      System.out.print(a[i]+" ");
+	}
+}
+}
